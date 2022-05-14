21645c1a1ffdd8d4c24324cb9625b70b533ffaac
79154472780@yandex.ru <79154472780@yandex.ru>
2022-05-14 17:59:04 +0300

    update version to 1.3

---
+++ src/main/java/ru/dwerd/keyboard/layout/transator/TransatorApplication.java
@@ -0,0 +1,13 @@
+package ru.dwerd.keyboard.layout.transator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/dwerd/keyboard/layout/transator/TransatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dwerd.keyboard.layout.transator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
